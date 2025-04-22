05a4d87774dd4d4ff64f83227de28c1003760d8b
Venugopal <venugopal2u@gmail.com>
2025-04-22 18:16:37 +0530

    Comment out stage_name in API Gateway deployment configuration

--- modules/api_gateway/api-gateway.tf
+++ modules/api_gateway/api-gateway.tf
@@ -36,7 +36,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 resource "aws_api_gateway_deployment" "greeting_deployment" {
   depends_on  = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.greeting_api.id
-  stage_name  = var.api_gateway_stage
+  #stage_name  = var.api_gateway_stage
 }
 
 resource "aws_api_gateway_stage" "dev_stage" {
